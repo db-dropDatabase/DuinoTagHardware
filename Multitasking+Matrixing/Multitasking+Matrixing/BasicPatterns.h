0758e4fa7b0cc41d2d12797f4b535c5e70f822ca
Noah <noah@koontzs.com>
2016-09-26 21:21:34 -0700

    and yet so far

--- Multitasking+Matrixing/Multitasking+Matrixing/BasicPatterns.h
+++ Multitasking+Matrixing/Multitasking+Matrixing/BasicPatterns.h
@@ -13,6 +13,8 @@
 #ifndef BASICPATTERNS_H_
 #define BASICPATTERNS_H_
 
+#define animationNum (sizeof(animationStore) / sizeof(animationStore[0]))
+
 typedef const uint8_t PROGMEM animation_t;
 
 //below are commands understood by the interpreter
@@ -23,23 +25,26 @@ enum lightCommands{
 	L_RAND_FLICKER = 254, //replace with a random number, but only 1 or 10
 };
 
-//an example, which I should probably explain better later:
-animation_t sample[] = {
-	L_SET_POWER, 10,  //turn the LED on
-	L_DELAY, 10,  //delay for 100 millis
-	L_SET_POWER, L_RAND_FLICKER, //set the power to a random number between 0 and 1
-	L_DELAY, 10, //delay for another 100 millis
- }; //and repeat
-
- animation_t flicker[] = {
-	L_SET_POWER, 10,
-	L_DELAY, L_RAND_FLICKER,
-	L_SET_POWER, 0,
- };
-
- animation_t dim[] = {
-	L_SET_POWER, L_RAND, 
-	L_DELAY, 10,
- };
+animation_t animationStore[][10] = { //array which stores the animations
+	{
+		L_SET_POWER, 10,  //turn the LED on
+		L_DELAY, 10,  //delay for 100 millis
+		L_SET_POWER, L_RAND, //set the power to a random number between 0 and 1
+		L_DELAY, 10, //delay for another 100 millis
+	}, //and repeat
+	{
+		L_SET_POWER, 10,
+		L_DELAY, L_RAND,
+		L_SET_POWER, 0,
+		L_DELAY, 5,
+	},
+	{
+		L_SET_POWER, 5,
+		L_DELAY, 100,
+		L_SET_POWER, 0,
+		L_DELAY, 5,
+	},
+	//insert as many as you like here
+};
 
 #endif /* BASICPATTERNS_H_ */
--- Multitasking+Matrixing/Multitasking+Matrixing/main.c
+++ Multitasking+Matrixing/Multitasking+Matrixing/main.c
@@ -9,88 +9,115 @@
 #include <stdbool.h>
 #include "BasicPatterns.h"
 
+#define F_CPU 8000000
+
 const uint8_t TICK_LEN = 100;
 
 void setLED(uint8_t LEDnum); //to be made later on
-uint8_t returnRandom(); //also to be made later
-bool returnRandomBool(); //same 
 
-struct LEDState_t {
-	uint16_t delaySetting,
-	uint8_t powerSetting,
-	uint8_t stepCounter,
+//TODO: split random into it's own file
+
+const uint8_t cheapRandom[] = {
+	10,6,5,1,3,3,8,10,5,6,3,4,5,7,3,5,8,5,8,4
 };
 
-void* animationQueue[] = {
-	&sample,
-	&flicker,
-	&dim
-};
+uint8_t randPoint = 0;
 
-LEDState_t LEDStates[sizeof(animationQueue)] = {};
-uint8_t LEDqueue[sizeof(animationQueue) * 10] = {};
-uint8_t nextLEDQueue[sizeof(animationQueue) * 10] = {};
+uint8_t returnRandom(){
+	if(randPoint == sizeof(cheapRandom)-1) randPoint = 0;
+	return cheapRandom[randPoint];
+}
+
+bool returnRandomBool(){
+	if(randPoint == sizeof(cheapRandom)-1) randPoint = 0;
+	return cheapRandom[randPoint]%2;
+}
+
+//TODO: Replace 3D array with uint32_t
+
+typedef struct {
+	uint16_t delaySetting;
+	uint8_t powerSetting;
+	uint8_t stepCounter;
+}LEDState_t;
+
+LEDState_t LEDStates[animationNum] = {};
+volatile bool LEDQueue[2][animationNum][10] = {};
+volatile uint8_t queueCounter = 0;
+volatile bool queueSwap = false;
+volatile uint8_t queuePointer = 0; 
 
 int main(void)
 {
-	//initialize ISR here
+	//internet code, to be modified
+	TCCR0A = (1 << WGM01);             //CTC mode
+	TCCR0B = (2 << CS00);              //div8
+	OCR0A = F_CPU/8 * 0.000010 - 1;    // 10us compare value
 
-	uint8_t queueCounter = 0;
+	//set all pins to output
+	DDRB = 0b11110;
 
     while (1) 
     {
-		//update virtual state machine
-		for(uint8_t i=0; i<sizeof(animationQueue); i++){
+		//for every animation
+		for(uint8_t i=0; i<animationNum; i++){
 			//if the animation is not in the middle of delaying, process the state machine
 			if(!LEDStates[i].delaySetting){
-				switch(animationQueue[i]->[LEDStates[i].stepCounter]){ //very complicated way of looking at the current step of the current animation
-				case L_SET_POWER:
-					//if its a flicker random, set power to the random flicker
-					if(animationQueue[i]->[LEDStates[i].stepCounter + 1] == L_RAND_FLICKER) LEDStates[i].powerSetting = returnRandomBool() * 10;
-					//if its a regular random, set power to random
-					else if(animationQueue[i]->[LEDStates[i].stepCounter + 1] == L_RAND) LEDStates[i].powerSetting = returnRandom();
-					//else set power to next item in animation
-					else LEDStates[i].powerSetting = animationQueue[i]->[LEDStates[i].stepCounter + 1];
-					//increment to next step
-					LEDStates[i].stepCounter += 2;
-					break;
-				case L_DELAY:
-					//if its a random delay, set delay to random * converstion factor
-					if(animationQueue[i]->[LEDStates[i].stepCounter + 1] == L_RAND) LEDStates[i].delaySetting = returnRandom() * (10000000 / TICK_LEN);
-					//else set the delay to the next value times the conversion factor
-					else LEDStates[i].delaySetting = animationQueue[i]->[LEDStates[i].stepCounter + 1] * (10000000 / TICK_LEN);
-					//increment to next step
-					LEDStates[i].stepCounter += 2;
-					break;
+				while(animationStore[i][LEDStates[i].stepCounter] != L_DELAY){
+					if(animationStore[i][LEDStates[i].stepCounter] == L_SET_POWER){
+						//if its a flicker random, set power to the random flicker
+						if(animationStore[i][LEDStates[i].stepCounter + 1] == L_RAND_FLICKER) LEDStates[i].powerSetting = returnRandomBool() * 10;
+						//if its a regular random, set power to random
+						else if(animationStore[i][LEDStates[i].stepCounter + 1] == L_RAND) LEDStates[i].powerSetting = returnRandom();
+						//else set power to next item in animation
+						else LEDStates[i].powerSetting = animationStore[i][LEDStates[i].stepCounter + 1];
+						//increment to next step
+						LEDStates[i].stepCounter += 2;
+
+						//set the next ten frames of the animation
+						for(uint8_t o=0; o<10; o++){
+							if(LEDStates[i].powerSetting > o) LEDQueue[queueSwap][i][o] = true;
+						}
+					}
+
+					//insert other instructions here
+
+					//check to make sure the animation hasn't ended, and if it has reset it
+					if(LEDStates[i].stepCounter >= sizeof(animationStore[i])) LEDStates[i].stepCounter = 0;
 				}
 
+				//the next step is now guaranteed to be a delay, so process that
+				//if its a random delay, set delay to random*converstion factor
+				if(animationStore[i][LEDStates[i].stepCounter + 1] == L_RAND) LEDStates[i].delaySetting = returnRandom() * (10000000 / TICK_LEN);
+				//else set the delay to the next value times the conversion factor
+				else LEDStates[i].delaySetting = animationStore[i][LEDStates[i].stepCounter + 1] * (10000000 / TICK_LEN);
+				//increment to next step
+				LEDStates[i].stepCounter += 2;
 				//check to make sure the animation hasn't ended, and if it has reset it
-				if(LEDStates[i].stepCounter >= sizeof(animationQueue[i])) LEDStates[i].stepCounter = 0;
+				if(LEDStates[i].stepCounter >= sizeof(animationStore[i])) LEDStates[i].stepCounter = 0;
 			}
+			//else if it is, dec. delay and copy last values (so the LED stays the same)
+			else{
+				for(uint8_t o=0; o<sizeof(LEDQueue[queueSwap][i]); o++) LEDQueue[queueSwap][i][o] = LEDQueue[!queueSwap][i][o];
+			}
+			//this always happens anyway
+			LEDStates[i].delaySetting--;
 		}
 
-		//now parse the state information into which LEDs to turn on when
-		for(uint8_t i=0; i<10; i++){
-			for(uint8_t j=0; j<sizeof(animationQueue); j++){
-				//if there is a delay setting, decrement it accordingly
-				if(LEDStates[i].delaySetting){
-					 LEDStates[i].delaySetting--;
-					 //make sure the LED doesn't change
-					 nextLEDQueue[queueCounter] = LEDqueue[queueCounter];
-				}
-				//if not, then if the dimmable setting is greater than the sub state
-				//set it to turn on
-				else if(LEDStates[i].powerSetting > i) nextLEDQueue[queueCounter] = j;
-				//else set it to not be on
-				else nextLEDQueue[queueCounter] = 0;
+		//enable ISR
+		TIMSK |= (1<<OCIE0A);
 
-				//increment queue
-				queueCounter++;
-			}
-		}
-
-		//if queue is full, wait until ISR empties it
-		while(queueCounter >= sizeof(nextLEDQueue));
+		//wait until ISR empties queue
+		while(queueCounter);
     }
 }
 
+ISR(TIMER0_COMPA_vect){
+	PORTB = (LEDQueue[queueSwap][0][queuePointer] << PB0) | (LEDQueue[queueSwap][1][queuePointer] << PB1) | (LEDQueue[queueSwap][2][queuePointer] << PB2);
+	queuePointer++;
+	if(queuePointer >= sizeof(LEDQueue[queueSwap][0])){
+		queuePointer = 0;
+		queueSwap = !queueSwap;
+		queueCounter = 0;
+	}
+}
