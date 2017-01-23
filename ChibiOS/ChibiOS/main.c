e8977afac418037068a3f3d9fd46463aa9259eec
HackBox <noah@koontzs.com>
2017-01-23 09:42:40 -0800

    YAY! Demo #1

--- ChibiOS/ChibiOS/main.c
+++ ChibiOS/ChibiOS/main.c
@@ -29,26 +29,28 @@ static THD_FUNCTION(Blinker1, arg) {
   }
 }
 
-static WORKING_AREA(waBlinker2, 40);
+static WORKING_AREA(waBlinker2, 48);
+static WORKING_AREA(waBlinker3, 48);
+static WORKING_AREA(waBlinker4, 48);
+static WORKING_AREA(waBlinker5, 48);
 static THD_FUNCTION(Blinker2, arg) {
-	
-	(void)arg;
 
 	while(TRUE){
 		uint8_t randNum;
 		returnRandom(&randNum);
 		
-		palSetPad(IOPORT2, *arg);
-		
-		chThdSleepMilliseconds(randNum + 135);
-		
-		palClearPad(IOPORT2, *arg);
-		
-		chThdSleepMilliseconds(235 - randNum);
+		while(randNum--){
+			palSetPad(IOPORT2, *(uint8_t *)arg);
+			
+			chThdSleepMicroseconds(randNum * 2);
+			
+			palClearPad(IOPORT2, *(uint8_t *)arg);
+			
+			chThdSleepMicroseconds(255 - (randNum * 2));
+		}
 		
 		returnRandom(&randNum);
-		
-		chThdSleepMilliseconds(randNum * 10);
+		chThdSleepMicroseconds((randNum % 50) * 600);
 	}
 	
 }
@@ -69,6 +71,7 @@ int main(void) {
   chSysInit();
 
   palClearPad(IOPORT2, PORTB_LED1);
+  palClearPad(IOPORT2, PB3);
 
   /*
    * Activates the serial driver 1 using the driver default configuration.
@@ -78,8 +81,17 @@ int main(void) {
   /*
    * Starts the LED blinker thread.
    */
+  const uint8_t pin1 = PB0;
+  const uint8_t pin2 = PB1;
+  const uint8_t pin3 = PB2;
+  const uint8_t pin4 = PB3;
+  
   chThdCreateStatic(waBlinker1, sizeof(waBlinker1), NORMALPRIO, Blinker1, NULL);
-  chThdCreateStatic(waBlinker2, sizeof(waBlinker2), NORMALPRIO, Blinker2, &PB2);
+  chThdCreateStatic(waBlinker2, sizeof(waBlinker2), NORMALPRIO, Blinker2, &pin1);
+  chThdCreateStatic(waBlinker3, sizeof(waBlinker3), NORMALPRIO, Blinker2, &pin2);
+  chThdCreateStatic(waBlinker4, sizeof(waBlinker4), NORMALPRIO, Blinker2, &pin3);
+  chThdCreateStatic(waBlinker5, sizeof(waBlinker5), NORMALPRIO, Blinker2, &pin4);
+  
 
   chnWrite(&SD1, (const uint8_t *)"Hello World!\r\n", 14);
   while(TRUE) {
