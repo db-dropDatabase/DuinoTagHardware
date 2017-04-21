f7d1ba4d993637d8b9372729f2f5553447ef052d
NotMac <noah@koontzs.com>
2017-04-21 09:47:55 -0700

    Need to fix deduping some more

--- LastFMPython/generate_history.py
+++ LastFMPython/generate_history.py
@@ -1,10 +1,12 @@
 import os
+from collections import namedtuple
 import yaml
+import json
 import pylast
-import pickle
+
 
 secrets_file = "LastFMPython\\secrets.yaml"
-listening_history_file = "LastFMPython\\history.bin"
+listening_history_file = "LastFMPython\\history.json"
 
 #get secrets from a file *not* on github
 def load_secrets():
@@ -16,71 +18,52 @@ def load_secrets():
         print("No secrets file found")
         quit()
 
+#class which makes the storing of my last.fm data a little safer and intuitive
+SmallTrack = namedtuple("SmallTrack", "title artist album timestamp_array")
+
 #get array of previous listening history from file
 def load_history():
-    with open(os.getcwd() + "\\" + listening_history_file, "r+b") as f:
-        return pickle.load(f)
+    history = []
+    with open(os.getcwd() + "\\" + listening_history_file, "r+") as f:
+        history = json.load(f)
+    for i, track in enumerate(history):
+        history[i] = SmallTrack(title = history[i][0], artist = history[i][1], album = history[i][2], timestamp_array = history[i][3])
+    return history
 
 #make history (no pun intendid)
 def write_history(history):
-    with open(os.getcwd() + "\\" + listening_history_file, "w+b") as f:
-        pickle.dump(history, f)
-
-#class which makes the storing of my last.fm data a little safer and intuitive
-class SmallTrack(object):
-    def __init__(self, track_name, track_artist, track_album, track_timestamp):
-        self.name = track_name
-        self.artist = track_artist
-        self.album = track_album
-        self.timestamp = [track_timestamp]
-
-    def get_name(self):
-        return self.name
-    
-    def get_artist(self):
-        return self.artist
-
-    def get_album(self):
-        return self.album
-    
-    def get_timestamp_array(self):
-        return self.timestamp
-    
-    def get_timestamp_recent(self):
-        return self.timestamp[0]
-
-    def add_timestamp(self, addTimestamp):
-        self.timestamp.append(addTimestamp)
-        self.timestamp.sort(reverse=True)
-
+    with open(os.getcwd() + "\\" + listening_history_file, "w+") as f:
+        json.dump(history, f, separators=(',', ': '), indent = True, sort_keys=True)
 
 secrets = load_secrets()
 
 #fire up API
 print("Logging in with: " + secrets["username"])
-network = pylast.LastFMNetwork(api_key = secrets["api_key"], api_secret = secrets["api_secret"],
-                                username = secrets["username"])
+network = pylast.LastFMNetwork(api_key = secrets["api_key"], api_secret = secrets["api_secret"], username = secrets["username"])
 
 #get the user you want to shuffle
 user = network.get_user(secrets["username"])
 print("Got user: " + user.get_name())
 
 #get the recent tracks of the user
-history = user.get_recent_tracks(limit = 0)
+history = user.get_recent_tracks(limit = 50)
 
 #put them into a friendly array, and dedupe while we're at it
 strippedHistory = []
 
 for track in history:
     #wrap track in better API
-    strippedTrack = SmallTrack(track.track.title, track.track.artist, track.album, track.timestamp)
+    strippedTrack = SmallTrack(title = track.track.title, artist = str(track.track.artist), album = str(track.album), timestamp_array = [track.timestamp])
     #check track against all stored tracks
     dupeFound = False
     for checkTrack in strippedHistory:
-        if(checkTrack.get_name() == strippedTrack.get_name()):
+        if(checkTrack.title == strippedTrack.title):
             #if we found a dupe, add the timestamp to the array
-            checkTrack.add_timestamp(strippedTrack.get_timestamp_recent())
+            checkTrack.timestamp_array.append(strippedTrack.timestamp_array[0])
+            checkTrack.timestamp_array.sort(reverse = True)
             dupeFound = True
+            print("Dupe Found: " + checkTrack.title)
+            print(checkTrack.timestamp_array)
             break
     
     if(dupeFound == False):
@@ -94,4 +77,4 @@ write_history(strippedHistory)
 
 strippedHistory = load_history()
 
-print(strippedHistory[0].get_name())
+print(strippedHistory[0].title)
